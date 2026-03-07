20e81bf02e10c540b4c979939e889e8fa27ff055
tamtam <tamtam113.tg@gmail.com>
2026-03-06 19:09:43 -0500

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
-import PfpPage from "./pages/Pfppage";   
+import PfpPage from "./pages/Pfppage";
 
 function App() {
+  const user = JSON.parse(localStorage.getItem("user")); // or however you store login
+
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* Home route */}
+        <Route
+          path="/"
+          element={user ? <Dashboard /> : <Auth />}
+        />
+
         <Route path="/auth" element={<Auth />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/pfp" element={<PfpPage />} />
+        <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/auth" />} />
+        <Route path="/pfp" element={user ? <PfpPage /> : <Navigate to="/auth" />} />
+
         <Route path="*" element={<NotFound />} />
+
       </Routes>
     </BrowserRouter>
   );
