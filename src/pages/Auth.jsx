4e77f6db9e6de7c789b67de3d2eee1f319b0970c
tamtam <tamtam113.tg@gmail.com>
2026-03-09 08:51:59 -0400

    y

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../index.css"; // ⭐
+import "../index.css"; 
 
 export default function Auth() {
   const [showLogin, setShowLogin] = useState(true);
