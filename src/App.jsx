dc33dc3f7f9e44391a2228219e99929c69500313
tamtam <tamtam113.tg@gmail.com>
2026-03-11 11:08:19 -0400

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 import Quiz from "./pages/Quiz";
+import PfpPage from "./pages/Pfppage"; 
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/auth" element={<Auth />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/quiz" element={<Quiz />} />
+        <Route path="/pfp" element={<PfpPage />} />
         <Route path="*" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
