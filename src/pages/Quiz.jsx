c6ad34022da51b787481ced231152fb79546e07b
tamtam <tamtam113.tg@gmail.com>
2026-03-10 21:20:41 -0400

    name not being renamed on github

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { quizQuestions } from "./quizData";
 import QuizResults from "./QuizResults";
-import "./Quizt.css";
+import "./Quiz.css";
 
 export default function Quiz() {
   const [answers, setAnswers] = useState([]);
